8b2c0310840154fd3cb05a89505a8a5a9bb4850c
Oleksandr Oleksiv <oleksiv1994@gmail.com>
2024-04-03 16:12:01 +0300

    * replace custom useFetch hook's usage with graphQL implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import {useState} from "react";
-import {Alert, CircularProgress, Input, List, ListItem, ListItemText} from "@mui/material";
+import {Alert, CircularProgress, Input, List, ListItemButton, ListItemText} from "@mui/material";
 import useDebounce from "./useDebounce.tsx";
 import useFetch from "./useFetch.tsx";
+import {gql, useQuery} from "@apollo/client";
+
+
+const GET_POKEMONS = gql`
+    query GetPokemons($name: String!) {
+        pokemons: pokemon_v2_pokemon(
+            order_by: { id: asc }
+            where: { name: { _iregex: $name } }
+        ) {
+            name
+            id
+        }
+    }
+`;
+
 
 function App() {
     const [pokemonName, setPokemonName] = useState("ditto");
     const debouncedPokemonName = useDebounce(pokemonName, 300);
-    const url = `https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0&${new URLSearchParams({
-        name: debouncedPokemonName,
-    })}`;
-    const { loading, error, data } = useFetch(url);
+    // const url = `https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0&${new URLSearchParams({
+    //     name: debouncedPokemonName,
+    // })}`;s
+    const { loading, error, data } = useQuery(GET_POKEMONS, {
+        variables: { name: debouncedPokemonName },
+    });
+    //const { loading, error, data } = useFetch(url);
 
     let filteredResults = [];
-    if (data && data.results) {
-        filteredResults = data.results.filter((pokemon) =>
+    if (data && data.pokemons) {
+        filteredResults = data.pokemons.filter((pokemon) =>
             pokemon.name.includes(debouncedPokemonName.toLowerCase())
         );
     }
@@ -30,9 +48,9 @@ function App() {
             {!loading && !error && filteredResults && (
                 <List>
                     {filteredResults.map((pokemon, index) => (
-                        <ListItem key={index}>
+                        <ListItemButton key={index}>
                             <ListItemText primary={pokemon.name} />
-                        </ListItem>
+                        </ListItemButton>
                     ))}
                 </List>
             )}
